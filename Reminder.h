10a3bef4532cd626e44082ea2d24c3d0378d8dc0
David Lowman <dlowman@gmail.com>
2010-12-01 21:47:34 -0700

    added reminder header file

---
+++ Reminder.h
@@ -0,0 +1,16 @@
+//
+//  Reminder.h
+//  remindful-base
+//
+//  Created by David Lowman on 11/27/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Reminder : NSObject {
+
+}
+
+@end
